4e976e6a2edde1e2e7e7aa9f0b2cbce6aa9821c3
Luz <luzvelascoreyes@gmail.com>
2025-10-13 18:38:09 -0500

    update 13/10

--- movil/practicas/AppTours-Backend/app.js
+++ movil/practicas/AppTours-Backend/app.js
@@ -1,6 +1,38 @@
 const express = require('express')
+const swaggerJsDoc = require('swagger-jsdoc') // npm i swagger-jsdoc
+const swaggerUi = require('swagger-ui-express') // npm i swagger-ui-express
+const cors = require('cors') // npm i cors
+const mysql = require('mysql2') // BD
+
+
 const app = express()
+app.use(express.json())
 const port = 3000
+const bd = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'v5090L03',
+    database: 'agencia'
+})
+
+// ------------------------ CONECTAR CON MYSQL ------------------------
+bd.connect((error) => {
+    if (error) {
+        console.log('ERROR al conectar: ' + error.stack)
+        return;
+    }
+    console.log('conectado a mysql [:')
+})
+
+// ------------------------ RUTA PARA MOSTRAR TODOS LOS TOURS ------------------------
+app.get('/tours', (req, res) => {
+    bd.query('SELECT * FROM Tours', (error, results) => {
+        if (error) {
+            console.log('error al ejecutar la consulta: ' + error.stack)
+        }
+        res.json(results)
+    })
+})
 
 app.get('/', (req, res) => {
     res.send('API de App Tours')
