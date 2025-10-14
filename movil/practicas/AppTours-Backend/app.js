de92d5980532ba4ab46226845f9a0c74ec3187c5
Luz <luzvelascoreyes@gmail.com>
2025-10-13 19:25:25 -0500

    update 13/10

--- movil/practicas/AppTours-Backend/app.js
+++ movil/practicas/AppTours-Backend/app.js
@@ -4,7 +4,6 @@ const swaggerUi = require('swagger-ui-express') // npm i swagger-ui-express
 const cors = require('cors') // npm i cors
 const mysql = require('mysql2') // BD
 
-
 const app = express()
 app.use(express.json())
 const port = 3000
@@ -25,6 +24,18 @@ bd.connect((error) => {
 })
 
 // ------------------------ RUTA PARA MOSTRAR TODOS LOS TOURS ------------------------
+
+/**
+ * @swagger
+ * /tours:
+ *  get:
+ *   summary: Listado de tours
+ *   tags: [Tours]
+ *   response:
+ *      200:
+ *          description: Listado de tours
+ */
+
 app.get('/tours', (req, res) => {
     bd.query('SELECT * FROM Tours', (error, results) => {
         if (error) {
@@ -37,6 +48,23 @@ app.get('/tours', (req, res) => {
 app.get('/', (req, res) => {
     res.send('API de App Tours')
 })
+
+
+// ------------------------ CONFIGURAR SWAGGER ------------------------
+const swagggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.1.0',
+        info: {
+            title: 'API de App Tours',
+            version: '1.0.0',
+            description: 'API de la app de tours'
+        },
+    },
+    apis: ['*.js'],
+}
+
+const swaggerDocs = swaggerJsDoc(swagggerOptions)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(port, () => {
     console.log('Servidor desde puerto ' + port)
